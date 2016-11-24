72cde1d1cc999f23cb127df186387060bc2e9ce3
Federico Pereiro <fpereiro@gmail.com>
2016-11-24 14:06:57 +0100

    Major release.

--- cocholate.js
+++ cocholate.js
@@ -1,9 +1,9 @@
 /*
-cocholate - v0.3.0
+cocholate - v1.0.0
 
 Written by Federico Pereiro (fpereiro@gmail.com) and released into the public domain.
 
-Please refer to readme.md to read the annotated source.
+Please refer to readme.md to read the annotated source (but not yet!).
 */
 
 (function () {
@@ -17,18 +17,202 @@ Please refer to readme.md to read the annotated source.
 
    var type   = teishi.t;
 
-   (function polyfill () {
+   // *** CORE ***
 
-      var createContainer = function () {
+   var c = window.c = function (selector, fun) {
+      if (teishi.stop ('c', ['fun', fun, ['function', 'undefined'], 'oneOf'])) return false;
+
+      var elements = c.find (selector);
+      if (elements === false) return false;
+      if (! fun) {
+         if (type (selector) === 'string' && selector.match (/^#[^\s,]+$/)) return elements [0];
+         else                                                               return elements;
+      }
+
+      var args = teishi.c (arguments).slice (2);
+      return dale.do (elements, function (v) {
+         return fun.apply (undefined, [v].concat (args));
+      });
+   }
+
+   c.nodeListToArray = function (nodeList) {
+      return dale.fil (nodeList, undefined, function (v, k) {
+         if (k.match (/^\d+$/)) return v;
+      });
+   }
+
+   c.setop = function (operation, set1, set2) {
+      if (operation === 'and') return dale.fil (set1, undefined, function (v) {
+         return set2.indexOf (v) !== -1 ? v : undefined;
+      });
+      var output = set1.slice ();
+      if (operation === 'or') {
+         dale.do (set2, function (v) {
+            if (output.indexOf (set2) === -1) output.push (v);
+         });
+      }
+      else {
+         if (output.length === 0) output = c.nodeListToArray (document.getElementsByTagName ('*'));
+         dale.do (set2, function (v) {
+            var index = output.indexOf (v);
+            if (index !== -1) output.splice (index, 1);
+         });
+      }
+      return output;
+   }
+
+   c.find = function (selector) {
+
+      if (type (selector) === 'string') return c.nodeListToArray (document.querySelectorAll (selector));
+
+      if (teishi.stop ('cocholate', [
+         ['selector', selector, ['array', 'string'], 'oneOf'],
+         function () {
+            return ['first element of array selector', selector [0], [':and', ':or', ':not'], 'oneOf', teishi.test.equal]
+         }
+      ])) return false;
+
+      var operation = selector.shift ();
+      var output = [];
+
+      dale.stop (selector, false, function (v, k) {
+         var elements = c.find (v);
+         if (elements === false) return output = false;
+         if (k === 0 && operation !== ':not') output = elements;
+         else         output = c.setop (operation.replace (':', ''), output, elements);
+      });
+      return output;
+   }
+
+   // *** DOM OPERATIONS ***
+
+   c.empty = function (selector) {
+      c (selector, function (element) {
+         dale.do (c.nodeListToArray (element.getElementsByTagName ('*')), function (v) {
+            v.remove ();
+         });
+      });
+   }
+
+   c.fill = function (selector, html) {
+      if (teishi.stop ('c.fill', ['html', html, 'string'])) return false;
+
+      c.empty (selector);
+      c (selector, function (element) {
+         element.innerHTML = html;
+      });
+   }
+
+   c.place = function (selector, where, html) {
+      if (teishi.stop ('c.place', [
+         ['where', where, ['beforeBegin', 'afterBegin', 'beforeEnd', 'afterEnd'], 'oneOf', teishi.test.equal],
+         ['html', html, 'string']
+      ])) return false;
+
+      c (selector, function (element) {
+         element.insertAdjacentHTML (where, html);
+      });
+   }
+
+   c.get  = function (selector, attributes, css) {
+      if (teishi.stop ('c.get', ['attributes', attributes, ['string', 'array'], 'oneOf'])) return false;
+
+      return c (selector, function (element) {
+         return dale.obj (attributes, function (v) {
+            if (css) return [v, element.style [v] || null];
+            else     return [v, element.getAttribute (v)];
+         });
+      });
+   }
+
+   c.set  = function (selector, attributes, css) {
+      if (teishi.stop ('c.set', [
+         ['attributes', attributes, 'object'],
+         [
+            ['attribute keys', 'start with an ASCII letter, underscore or colon, and be followed by letters, digits, underscores, colons, periods, dashes, extended ASCII characters, or any non-ASCII characters.'],
+            dale.keys (attributes),
+            /^[a-zA-Z_:][a-zA-Z_:0-9.\-\u0080-\uffff]*$/,
+            'each', teishi.test.match
+         ],
+         ['attribute values', attributes, ['integer', 'string', 'null'], 'eachOf']
+      ])) return false;
+
+      c (selector, function (element) {
+         dale.do (attributes, function (v, k) {
+            if       (css)        element.style [k] = v === null ? '' : v;
+            else if  (v === null) element.removeAttribute (k);
+            else                  element.setAttribute    (k, v);
+         });
+      });
+   }
+
+   // *** NON-DOM OPERATIONS ***
+
+   c.ready = function (fun) {
+      if (document.addEventListener) return document.addEventListener ('DOMContentLoaded', fun);
+      // http://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
+      if (document.attachEvent) {
+         document.attachEvent ('onreadystatechange', function () {
+            if (document.readyState === 'complete') fun ();
+         });
+      }
+      else fun ();
+   }
+
+   c.cookie = function (cookie) {
+      return dale.obj ((cookie || document.cookie).split (/;\s+/), function (v) {
+         if (v === '') return;
+         v = v.split ('=');
+         var name = v [0];
+         var value = v.slice (1).join ('=');
+         return [name, value];
+      });
+   }
+
+   c.ajax = function (method, path, body, headers, callback) {
+      method   = method   || 'GET';
+      body     = body     || '';
+      headers  = headers  || {};
+      callback = callback || function () {};
+      if (teishi.stop ('c.ajax', [
+         ['method',   method,   'string'],
+         ['path',     path,     'string'],
+         ['headers',  headers,  'object'],
+         ['callback', callback, 'function']
+      ])) return false;
+
+      var r = new XMLHttpRequest ();
+      r.open (method.toUpperCase (), path, true);
+      if (teishi.complex (body) && teishi.t (body, true) !== 'formdata') {
+         headers ['content-type'] = headers ['content-type'] || 'application/json';
+         body = teishi.s (body);
+      }
+      dale.do (headers, function (v, k) {
+         r.setRequestHeader (k, v);
+      });
+      r.onreadystatechange = function () {
+         if (r.readyState !== 4) return;
+         if (r.status !== 200) callback (r);
+         else                  callback (null, r);
+      }
+      r.send (body);
+      return {headers: headers, body: body};
+   }
+
+   // *** POLYFILL ***
+
+   c.polyfill = function () {
+
+      var newElement = function () {
          return document.createElement ('_');
       }
 
       // https://gist.github.com/eligrey/1276030
-      if (! createContainer ().insertAdjacentHTML) {
+      if (! newElement ().insertAdjacentHTML) {
 
          HTMLElement.prototype.insertAdjacentHTML = function (position, html) {
 
-            var This = this, container = createContainer (), Parent = This.parentNode, node, firstChild, nextSibling;
+            var This = this, container = newElement (), Parent = This.parentNode, node, firstChild, nextSibling;
 
             container.innerHTML = html;
 
@@ -56,160 +240,16 @@ Please refer to readme.md to read the annotated source.
          }
       }
 
-   }) ();
-
-   var c = window.c = function (selector, fun) {
-      if (teishi.stop ('c', ['fun', fun, ['function', 'undefined'], 'oneOf'])) return false;
-      var elements = c.find (selector);
-      if (elements === false) return false;
-      if (! fun) return type (selector) === 'string' && selector [0] === '#' ? elements [0] : elements;
-      var Arguments = teishi.c (arguments).slice (2);
-      return dale.do (elements, function (v) {
-         return fun.apply (undefined, [v].concat (Arguments));
-      });
-   }
-
-   c.nodeListToArray = function (nodeList) {
-      var length = nodeList.length, i = 0, output = [];
-      while (i < length) {
-         output [i] = nodeList [i++];
+      if (! Array.prototype.indexOf) {
+         Array.prototype.indexOf = function (element, from) {
+            var result = dale.stopNot (from ? this.slice (from) : this, undefined, function (v, k) {
+               if (element === v) return k;
+            });
+            return result === undefined ? -1 : result;
+         }
       }
-      return output;
    }
 
-   // *** DOM OPERATIONS ***
-
-   c.setop = function (operation, set1, set2) {
-      if (operation === 'and') return dale.fil (set1, undefined, function (v) {
-         return set2.indexOf (v) !== -1 ? v : undefined;
-      });
-      var output = set1.slice ();
-      if (operation === 'or') {
-         dale.do (set2, function (v) {
-            if (output.indexOf (set2) === -1) output.push (v);
-         });
-      }
-      else {
-         if (output.length === 0) output = dale.do (c.nodeListToArray (document.getElementsByTagName ('*')), function (v) {return v});
-         dale.do (set2, function (v) {
-            var index = output.indexOf (v);
-            if (index !== -1) output.splice (index, 1);
-         });
-      }
-      return output;
-   }
-
-   c.find = function (selector) {
-
-      var selectorType = type (selector);
-      if (selectorType === 'string') return c.nodeListToArray (document.querySelectorAll (selector));
-
-      if (teishi.stop ('cocholate', [
-         ['selector type', selectorType, ['array', 'object'], 'oneOf', teishi.test.equal],
-         [selectorType === 'object', ['keys of selector', dale.keys (selector), ['or', 'not'], 'eachOf', teishi.test.equal]]
-      ])) return false;
-
-      var output = [];
-      dale.stop (selector, false, function (v, k) {
-         var element = c.find (v);
-         if (element === false) return output = false;
-         output = c.setop (selectorType === 'array' ? 'and' : k, output, element);
-      });
-      return output;
-   }
-
-   c.empty = function (selector) {
-      c (selector, function (element) {
-         dale.do (c.nodeListToArray (element.getElementsByTagName ('*')), function (v) {
-            v.remove ();
-         });
-      });
-   }
-
-   c.fill = function (selector, html) {
-      c.empty (selector);
-      c (selector, function (element) {
-         element.innerHTML = html;
-      });
-   }
-
-   c.get  = function (selector, attributes, css) {
-      if (teishi.stop ('c.get', ['attributes', attributes, ['string', 'array'], 'oneOf'])) return false;
-
-      return c (selector, function (element) {
-         return dale.obj (attributes, function (v) {
-            if (css) return [v, element.style [v]];
-            else     return [v, element.getAttribute (v)];
-         });
-      });
-   }
-
-   c.set  = function (selector, attributes, css) {
-      if (teishi.stop ('c.set', [
-         ['attributes', attributes, 'object'],
-         ['attribute values', attributes, ['integer', 'string', 'null'], 'eachOf']
-      ])) return false;
-
-      c (selector, function (element) {
-         dale.do (attributes, function (v, k) {
-            if       (css)        element.style [k] = v === null ? '' : v;
-            else if  (v === null) element.removeAttribute (k);
-            else                  element.setAttribute    (k, v);
-         });
-      });
-   }
-
-   c.place = function (selector, where, html) {
-      if (teishi.stop ('c.place', [
-         ['where', where, ['beforeBegin', 'afterBegin', 'beforeEnd', 'afterEnd'], 'oneOf', teishi.test.equal],
-         ['html', html, 'string']
-      ])) return false;
-      c (selector, function (element) {
-         element.insertAdjacentHTML (where, html);
-      });
-   }
-
-   c.ready = function (fun) {
-      if (document.addEventListener) return document.addEventListener ('DOMContentLoaded', fun);
-      // http://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
-      if (document.attachEvent) {
-         document.attachEvent ('onreadystatechange', function () {
-            if (document.readyState === 'complete') fun ();
-         });
-      }
-      else fun ();
-   }
-
-   // *** NON-DOM OPERATIONS ***
-
-   c.cookie = function () {
-      return dale.obj (document.cookie.split (/;\s+/), function (v) {
-         if (v === '') return;
-         v = v.split ('=');
-         var name = v [0];
-         var value = v.slice (1).join ('=');
-         return [name, value];
-      });
-   }
-
-   c.ajax = function (method, path, body, headers, callback) {
-      headers = headers || {};
-      callback = callback || function () {};
-      var r = new XMLHttpRequest ();
-      r.open (method.toUpperCase (), path, true);
-      if (teishi.complex (body)) {
-         headers ['content-type'] = headers ['content-type'] || 'application/json';
-         body = teishi.s (body);
-      }
-      dale.do (headers, function (v, k) {
-         r.setRequestHeader (k, v);
-      });
-      r.onreadystatechange = function () {
-         if (r.readyState !== 4) return;
-         if (r.status !== 200) callback (r);
-         else                  callback (null, r);
-      }
-      r.send (body);
-   }
+   c.polyfill ();
 
 }) ();
--- readme.md
+++ readme.md
@@ -2,21 +2,14 @@
 
 > "Why cocholate? Because it goes well with [vanilla](http://vanilla-js.com)." -- cocholate's PR.
 
-cocholate is a small library for DOM manipulation. It assumes that you either will use it in modern browsers or with a polyfill. It's meant to be small, easily understandable and fast.
+cocholate is a small library for DOM manipulation. It's meant to be small, easily understandable and fast.
 
-cocholate is very much a work in progress. Feedback is very welcome (fpereiro@gmail.com).
+## Current status of the project
 
-## In lieu of readme
+The current version of cocholate, v1.0.0, is considered to be *unstable* and *incomplete*. [Suggestions](https://github.com/fpereiro/cocholate/issues) and [patches](https://github.com/fpereiro/cocholate/pulls) are welcome. Future changes planned are:
 
-selector:
-   - string: CSS selector (`#` for id, `.` for class, by tagname, `[attribute]`, `[attribute="value"]`, or any other valid CSS selector)
-   - array (representing AND), every value a selector (string or otherwise)
-   - object (representing OR and/or NOT, depending on the keys), every value a selector
-
-array selector: ANDs all possibilities, which means that takes all intersections.
-object selector: can contain at most two keys, `or`, `not`. The values of these keys must be selectors.
-
-operators are processed in the order they are read!
+- Improve the readme and add annotated source code.
+- Test the library in older browsers and improve the polyfill.
 
 ## Installation
 
@@ -36,12 +29,295 @@ cocholate is written in Javascript. You can use it in the browser by sourcing th
 Or you can use these links to use the latest version - courtesy of [RawGit](https://rawgit.com) and [MaxCDN](https://maxcdn.com).
 
 ```html
-<script src="https://cdn.rawgit.com/fpereiro/dale/2208a574ac37037575ae0ee4260b1b0c5062eede/dale.js"></script>
-<script src="https://cdn.rawgit.com/fpereiro/teishi/ac4fb7f946f11a8fdc24db64bb5ff55b26adeba0/teishi.js"></script>
-<script src="https://cdn.rawgit.com/fpereiro/cocholate/ffb9e00c97f61130c9f8dba6254b0c1d3e288c6e/cocholate.js"></script>
+<script src="https://cdn.rawgit.com/fpereiro/dale/1bb6973037dd409f667231d51c55845672d19821/dale.js"></script>
+<script src="https://cdn.rawgit.com/fpereiro/teishi/984e9295f7ef31cd04576b8f9ac015e1953aabc1/teishi.js"></script>
+<script src=""></script>
 ```
 
-cocholate is an client-side library exclusively. Still, you can find it in npm: `npm install cocholate`
+cocholate is exclusively a client-side library. Still, you can find it in npm: `npm install cocholate`
+
+## Loading cocholate
+
+As soon as you include cocholate, it will be available on `window.c`.
+
+```javascript
+var c = window.c ();
+```
+
+cocholate automatically executes its own polyfill (available at `c.polyfill`). If you want to override it, you can simply load your own polyfill before loading cocholate.
+
+For the rest of the readme we'll assume that you created your instance of cocholate and named it `c`.
+
+## Selectors
+
+`c` is the main function of the library. It takes a `selector` and an optional `fun`.
+
+Let's go with the simplest case:
+
+```javascript
+// This code will return an array with all the divs in the document.
+c ('div');
+```
+
+```javascript
+// This code will return an array with all the elements that have the class .nav.
+c ('.nav');
+```
+
+Whenever you pass a string as a `selector` and no other arguments, cocholate simply uses the native [document.querySelectorAll](http://www.w3schools.com/jsref/met_document_queryselectorall.asp), with the sole difference that returns an array instead of a [NodeList](http://www.w3schools.com/jsref/met_document_queryselectorall.asp).
+
+If you want to use the logical operations `and`, `not` and `or`, you can do so by using a `selector` that's an array where the first element is either `':and'`, `':or'` or `':not'`.
+
+
+```javascript
+// This code will return an array with all the elements that are `div` or `p`
+c ([':or', 'div', 'p']);
+
+// This code will return an empty array (because there are no elements that can be simultaneously a `div` and a `p`).
+c ([':and', 'div', 'p']);
+
+// This code will return an array with all the elements that are neither `div` or `p`.
+c ([':not', 'div', 'p']);
+```
+
+You can also nest the selectors to an arbitrary degree, as long as the first element of each nested array selector is one of `':and'`, `':or'` or `':not'`:
+
+```javascript
+// This code will return an array with all the elements that are `div` or `p` and are contained inside `body`.
+c ([':and', 'body *', [':or', 'div', 'p']]);
+```
+
+A subtle point: when you pass multiple elements to a `':not'` selector, it is actually equivalent to using the `':or'` selector. For example, `[':not', 'div', 'p']` is equivalent to writing `[':not', [':or', 'div', 'p']]`. The reason for this disambiguation is that `and` and `or` are operations on two operands, where `not` is an unary operation. The choice of `or` instead of `and` reflects what I believe is the most intuitive and common usage of `not` for multiple operands.
+
+```javascript
+// These two calls will return the same result.
+c ([':not', 'div', 'p']);
+c ([':not', [':or', 'div', 'p']]);
+```
+
+## `fun`
+
+Besides returning an array of DOM elements, we will want to do some operations on them. To do this, we can pass a second argument to `c`, which is a function that will be executed for every element that matched the selector. The results will be collected on an array that's then returned.
+
+For example, the following call will return an array with the ids of all the `divs` in the document.
+
+```javascript
+c ('div', function (e) {
+   return e.getAttribute ('id');
+});
+```
+
+All the following DOM functions are implemented as underlying calls to `c`, passing its specific logic as the second argument to `c`.
+
+## DOM functions
+
+All the functions presented in this section take a `selector` as its first element and execute its logic for each of the elements matching the selector.
+
+### `c.empty`
+
+`c.empty` [removes](https://www.tutorialspoint.com/prototype/prototype_element_remove.htm) all the DOM elements within the elements matched by the selector. In other words, it completely gets rid of all the DOM elements nested inside of the matching elements. This function has no meaningful return value.
+
+### `c.fill`
+
+`c.fill` takes `html` (an HTML string) as its second argument, and first empties the element and then fills it with the provided HTML string. This function has no meaningful return value.
+
+### `c.place`
+
+`c.place` takes `where` as its second argument and `html` as its third. `where` can be one of `'beforeBegin'`, `'afterBegin'`, `'beforeEnd'`, `'afterEnd'`, and `html` is an HTML string. This function has no meaningful return value. Its functionality is based on that of [insertAdjacentHTML](https://msdn.microsoft.com/en-us/library/ms536452(v=vs.85).aspx).
+
+To explain what this function does, an example serves best. If you start with the following HTML:
+
+```html
+<div id="vamo">
+</div>
+```
+
+If you would execute the following function call:
+
+```javascript
+c.place ('#vamo', 'beforeBegin', '<p>beforeBegin</p>');
+```
+
+You would end up with the following HTML.
+
+```html
+<p>beforeBegin</p>
+<div id="vamo">
+</div>
+```
+
+If you then do the following three calls:
+
+```javascript
+c.place ('#vamo', 'afterBegin', '<p>beforeBegin</p>');
+c.place ('#vamo', 'beforeEnd', '<p>beforeEnd</p>');
+c.place ('#vamo', 'afterEnd', '<p>afterEnd</p>');
+```
+
+You will get:
+
+```html
+<p>beforeBegin</p>
+<div id="vamo">
+<p>afterBegin</p>
+<p>beforeEnd</p>
+</div>
+<p>afterEnd</p>
+```
+
+### `c.get`
+
+`c.get` is useful for fetching attributes from elements. It takes `attributes` as its second argument (which can be either a string or an array of strings, each of them representing an attribute name) and an optional boolean third parameter `css` which marks whether you want to get CSS properties instead of DOM ones.
+
+For each of the matching elements, this function will return an object where the key is the attribute name and the corresponding value is the attribute value.
+
+For example, if you have the following HTML:
+
+```html
+<p id="a" class="red"></p>
+<p id="b" class="blue"></p>
+<p id="c" class="green"></p>
+```
+
+And you run this code:
+
+```javascript
+c.get ('p', 'class');
+```
+
+You'll get an array with three objects: `[{class: 'red'}, {class: 'blue'}, {class: 'green'}]`.
+
+If you run this code:
+
+```javascript
+c.get ('p', ['id', 'class']);
+```
+
+You'll get an array with three objects but two properties each: `[{id: 'a', class: 'red'}, {id: 'b', class: 'blue'}, {id: 'c', class: 'green'}]`.
+
+Using the `css` attribute, you can obtain the CSS properties of an element. Consider this example:
+
+```html
+<p style="color: red;"></p>
+<p style="color: blue;"></p>
+<p style="color: green;"></p>
+```
+
+```javascript
+c.get ('p', 'color', true);
+```
+
+If you run this code on the above HTML, you will obtain an array with three objects: `[{color: 'red'}, {color: 'blue'}, {color: 'green'}]`.
+
+Finally, either with normal attributes or CSS ones, if the attribute is not present, you will get `null` as its value. For example:
+
+```javascript
+c.get ('p', 'name');         // will return `{name: null}`
+c.get ('p', 'height', true); // will return `{height: null}`
+```
+
+### `c.set`
+
+This function is similar to `c.get`, except that it *sets* the attributes instead of getting them.
+
+This function takes a selector as first argument, and as second argument an object with all the properties you wish to set. An optional `css` flag is the third argument, in order to set inline CSS properties.
+
+If you have the following HTML:
+
+```html
+<p>
+</p>
+```
+
+```javascript
+c.set ('p', {class: 'someclass'});
+```
+
+The HTML will look like this:
+
+```html
+<p class="someclass">
+</p>
+```
+
+To remove an attribute, just pass `null` as the attribute value. If you execute this code:
+
+```javascript
+c.set ('body p', {class: null});
+```
+
+The HTML will go back to its original state:
+
+```html
+<p>
+</p>
+```
+
+If you pass a truthy third argument, you'll set/unset CSS properties instead.
+
+```javascript
+c.set ('body p', {color: 'red'}, true);
+```
+
+Now the HTML will look like this:
+
+```html
+<p style="color: red;">
+</p>
+```
+
+To remove the style property, you can also use `null` as a value:
+
+```javascript
+c.set ('body p', {color: null}, true);
+```
+
+Now the HTML will look like this:
+
+```html
+<p style="">
+</p>
+```
+
+## Non-DOM functions
+
+Besides the five functions for DOM manipulation, there are three more for a few things that are convenient to have around.
+
+### `c.ready`
+
+A function that gets executed when the DOM has finished loading. It has its own polyfill. Takes a single argument, `fun`, containing the code to be executed when this event happens.
+
+### `c.cookie`
+
+Quick & dirty cookie parsing. Takes an optional argument, `cookie`, a cookie string that will be parsed. If you don't pass any arguments, this function will read the cookie at `document.cookie` instead.
+
+This function returns an object with keys/values, each of them pertaining to a property of the cookie.
+
+### `c.ajax`
+
+This function can make ajax calls and provides a few conveniences.
+
+It takes five arguments:
+
+- `method`: a string. Defaults to `'GET'` if you pass a falsy argument.
+- `path`: a string with the target path.
+- `body`: the body of the request. Defaults to `''` if you pass a falsy argument.
+- `headers`: an object where every value is a string. Defaults to `{}` if you pass a falsy argument.
+- `callback`: a function to be executed after the request is completed. Defaults to an empty function if you pass a falsy argument.
+
+The conveniences provided are:
+
+- You can directly pass a [FormData](https://developer.mozilla.org/en-US/docs/Web/API/FormData) object, useful for `multipart/form-data` requests.
+- If you pass an array or object as `body`, the `content-type` header will be automatically set and the body will be stringified.
+- All ajax requests done through this function are asynchronous.
+- The function will synchronously return an object of the form `{headers: ..., body: ...}`.
+- The callback will unconditinally receive the request itself as its first argument. This request contains all the relevant information, including payloads and errors.
+
+## Source code
+
+The complete source code is contained in `cocholate.js`. It is about 260 lines long.
+
+Annotated source code will be forthcoming when the library stabilizes.
 
 ## License
 
